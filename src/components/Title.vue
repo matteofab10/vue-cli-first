f104af69d543eeda6c7aeb57503936e03cefe28c
matteofab10 <matteofabio10@gmail.com>
2021-11-23 15:38:07 +0100

    creazione e fine progetto senza bonus

---
+++ src/components/Title.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>“La mia prima app con Vue CLI!”</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Title'
+  
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    color: blue;
+  }
+
+  .container {
+    background-color: green;
+  }
+</style>
